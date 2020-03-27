058b311778d68b6136337ab01c4a732865a2def1
Natalia <natikrak@gmail.com>
2020-03-27 16:14:48 +0100

    2 metody

--- src/main/java/zadania/Zadanie3.java
+++ src/main/java/zadania/Zadanie3.java
@@ -2,16 +2,14 @@ package zadania;
 
 public class Zadanie3 {
 
-    private float basePrice;
-    private int discount;
-    private float priceWithDiscount;
-    private int tax;
-    private float finalPrice;
-    private String city;
-
+    private static float basePrice;
+    private static int discount;
+    private static float priceWithDiscount;
+    private static int tax;
+    private static float finalPrice;
+    private static String city;
 
     public static void metoda1() {
-
 
         //obliczanie discount
         if (basePrice < 100) {
@@ -31,4 +29,39 @@ public class Zadanie3 {
         priceWithDiscount = (((100 - discount) / 100)) * basePrice));
         priceWithTax = priceWithDiscount + ((tax / 100) + priceWithDiscount);
     }
+
+
+    private static void printRandomList() {
+
+        System.out.println("To jest super program");
+
+        int min = 0;
+        int max = 10;
+        List<Integer> lista = new ArrayList<>();
+        Random r = new Random();
+        for (int i = 0; i < 10; i++) {
+            int wylosowanaLiczba = r.nextInt((max - min) + 1) + min;
+            lista.add(wylosowanaLiczba);
+        }
+        System.out.println(lista);
+
+        lista.add(priceWithDiscount);
+        lista.add
+    }
+
+
+
+        private static void printList() {
+            System.out.println("To jest niesamowity program");
+
+            int min = 0;
+            int max = 10;
+            List<Integer> lista = new ArrayList<>();
+            Random r = new Random();
+            for (int i = 0; i < 10; i++) {
+                int wylosowanaLiczba = r.nextInt((max - min) + 1) + min;
+                lista.add(wylosowanaLiczba);
+            }
+            System.out.println(lista);
+        }
 }
