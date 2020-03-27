b169c1078f3a808b0ad66b1eb81bff265dade149
Natalia <natikrak@gmail.com>
2020-03-27 15:58:04 +0100

    dluga metoda - pierwsze zadanie

--- src/main/java/zadania/Zadanie3.java
+++ src/main/java/zadania/Zadanie3.java
@@ -1,4 +1,34 @@
 package zadania;
 
 public class Zadanie3 {
+
+    private float basePrice;
+    private int discount;
+    private float priceWithDiscount;
+    private int tax;
+    private float finalPrice;
+    private String city;
+
+
+    public static void metoda1() {
+
+
+        //obliczanie discount
+        if (basePrice < 100) {
+            discount = 5;
+
+        } else {
+            if (city.equals("Katowice")) {
+                discount = 15;
+            } else discount = 10;
+        }
+        // obliczanie podatku
+        if (!city.equals("Katowice")) {
+            tax = 23;
+        }
+
+        //obliczanie ceny koncowej
+        priceWithDiscount = (((100 - discount) / 100)) * basePrice));
+        priceWithTax = priceWithDiscount + ((tax / 100) + priceWithDiscount);
+    }
 }
