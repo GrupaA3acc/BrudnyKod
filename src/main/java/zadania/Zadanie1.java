42e5c4f377a4fe7478a27d365e07c190c9090210
Natalia <nszarek284@gmail.com>
2020-03-29 12:51:42 +0200

    Bardzo dluga funkcja (poprawiona)

--- src/main/java/zadania/Zadanie1.java
+++ src/main/java/zadania/Zadanie1.java
@@ -1,74 +1,43 @@
 package zadania;
 
-import java.util.*;
-
 public class Zadanie1 {
 
+
     public static void main(String[] args) {
-        liczenieJakisRzeczy(2,5,1);
+        metoda1(2,1,1);
     }
+    //Jeśli figura jest kołem funkcja szuka punktów calkowite dla okregu (wewnatrz i na "brzegu")
 
-    //funkcja szuka punktów calkowite dla okregu (wewnatrz i na "brzegu")
-
-    //1 - kolo (plaska)
+    //1 - koło (płaska)
     //2 - walec
-    //3 - stozek
+    //3 - stożek
 
-    //Sorki, ze takie dlugie
-
-    public static void liczenieRoznychRzeczyDlaBryl(double r, double h, int numer)
+    public static void metoda1(double r, double h, int numer)
     {
         //liczenie pola podstawy
         double Pp = Math.PI*r*r;
         double V = 0.0;
 
         //sprawdzanie, czy figura jest kołem. Dla koła funkcja szuka punktów całkowitych, które znajdują się w kole.
-        if(numer == 1)
-        {
-            List<Integer> listX = new ArrayList<>();
-            List<Integer> listY = new ArrayList<>();
-
+        if(numer == 1) {
             int x = 0;
             int y = 0;
-
             //szukanie punktów calkowitych w kole i dodawanie ich do tablicy
             while(x<= r)
             {
                 while(y<=r)
                 {
                     if(Math.sqrt(x*x + y*y) <= r) {
-
                         if (x != 0 && y != 0) {
-                            //pieprwsza cwiartka
-                            listX.add(x);
-                            listY.add(y);
-
-                            //druga cwiartka
-                            listX.add(-x);
-                            listY.add(y);
-
-                            //trzecia cwiartka
-                            listX.add(-x);
-                            listY.add(-y);
-
-                            //czwartka cwiartka
-                            listX.add(x);
-                            listY.add(-y);
-                        }
-                        else if(x != 0 || y != 0)
-                        {
-                            listX.add(x);
-                            listY.add(y);
-
-                            if(x == 0)
-                            {
-                                listX.add(x);
-                                listY.add(-y);
-                            }
-                            else {
-                                listX.add(-x);
-                                listY.add(y);
-                            }
+                            System.out.println(x + " "+ y); //pierwsza cwiartka
+                            System.out.println(-x + " " + y); //druga cwiartka
+                            System.out.println(-x + " " + -y); //trzecia cwiartka
+                            System.out.println(x + " " + -y); //czwarta cwiartka
+                        } else if (x != 0 || y != 0) {
+                            System.out.println(x + " " + y);
+                            if (x == 0)
+                                System.out.println(x + " "+ -y);
+                            else System.out.println(-x + " "+ y);
                         }
                         y++;
                     }
@@ -78,36 +47,19 @@ public class Zadanie1 {
                 x++;
                 y=0;
             }
-            listX.add(0);
-            listY.add(0);
-
-
-            for(int i=0;i<listX.size();i++)
-            {
-                System.out.println(listX.get(i) + " " + listY.get(i));
-            }
+            System.out.println("0 0");
         }
-
         //liczenie objetosci dla bryl
         else
         {
             if(numer == 2)
-            {
                 V = Pp * h;
-
-            }
             else if(numer == 3)
-            {
                 V = (Pp * h)/3;
-            }
-
             else
                 System.out.println("Wpisano numer, dla którego nie przydzielono figury");
             System.out.println(V);
         }
-
     }
-
-
 
 }
